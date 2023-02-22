1c9cf4d37d8ea3ef89fce23e4e86a2d609ba9f46
Cristiano-woody <cristiano.nog.woody@gmail.com>
2023-02-22 01:38:57 -0300

    add weatherforecast

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,6 +2,7 @@ import CardsProjects from './CardsProjects'
 import Styles from './Styles/Projects.module.css'
 import ProjectPortifolio from './assets/ProjectPortifolio.png'
 import PaginaEmConstrucao from './assets/pagina_em_construcao.png'
+import weatherForecast from './assets/projetoWeatherForecast.png'
 
 function Projects() {
     return (
@@ -21,9 +22,13 @@ function Projects() {
                         urlacessprop='https://cristiano-woody.github.io/Cristiano-Nogueira/'
                     />
                     <CardsProjects
-                        textDescriptionprop='Projeto em construção.'
-                        title='em construção'
-                        imgbackground={PaginaEmConstrucao}
+                        title='weather forecast'
+                        textDescriptionprop='weather Forecast é uma aplicação web na qual exibe dados meteorológicos de cidades, bairros, estados e países do mundo naquele momento.
+                        O projeto weather forecast foi construído com o intuito de exercitar a manipulacão de requisições juntamente com a manipulação do DOM.'
+                        imgbackground={weatherForecast}
+                        usedtoolsprop='Vanilla JavaScrip, HTML5, CSS3.'
+                        urlRepositoryprop='https://github.com/Cristiano-woody/previs-o-do-tempo'
+                        urlacessprop='https://cristiano-woody.github.io/previs-o-do-tempo/'
                     />
                     <CardsProjects
                         textDescriptionprop='Projeto em construção.'
